28c34d9805c31040ce9388702103259e39dee037
Max Igor <maxigor.ferreira@gmail.com>
2014-10-16 20:16:24 -0300

    modifications on README.md

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,8 +1,21 @@
 app.controller("mainController", function($scope, $http){
 
+    $scope.apiKey = "1e20f306004e9fda7adff716375f1fba";
+    $scope.results = [];
     $scope.init = function() {
+        var today = new Date();
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 5 + '/?callback=JSON_CALLBACK').success(function(data) {
 
+            angular.forEach(data, function(value, index){
+                var date = value.date;
+                angular.forEach(value.episodes, function(tvshow, index){
+                    tvshow.date = date;
+                    $scope.results.push(tvshow);
+                });
+            });
+        }).error(function(error) {
+
+        });
     };
-
 });
-
